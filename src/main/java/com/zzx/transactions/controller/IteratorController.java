80af54f49152032e16672ee671a69ed521abec32
husky <1101648204@qq.com>
2020-05-13 22:53:28 +0800

    容器思想

--- src/main/java/com/zzx/transactions/controller/IteratorController.java
+++ src/main/java/com/zzx/transactions/controller/IteratorController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,14 +18,14 @@ public class IteratorController {
     @Autowired
     private IteratorService iteratorService;
 
-    private SimpleDateFormat date=new SimpleDateFormat("yyyy-MM-dd");
-    private SimpleDateFormat time=new SimpleDateFormat("HH:mm:ss");
-    private SimpleDateFormat dateTime=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+    private SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+    private SimpleDateFormat dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @GetMapping("/iterator")
-    public void insertMap(){
-        Map<String,BaseDO> map =new ConcurrentHashMap<>();
-        TradeDO one=new TradeDO();
+    public void insertMap() {
+        Map<String, BaseDO> map = new ConcurrentHashMap<>();
+        TradeDO one = new TradeDO();
         one.setId(1);
         one.setTradeDate(date.format(System.currentTimeMillis()));
         one.setTradeTime(time.format(System.currentTimeMillis()));
@@ -38,7 +37,7 @@ public class IteratorController {
         one.setBankAccount("zzx");
         one.setInsertime(dateTime.format(System.currentTimeMillis()));
 
-        TradeDO two=new TradeDO();
+        TradeDO two = new TradeDO();
         two.setId(2);
         two.setTradeDate(date.format(System.currentTimeMillis()));
         two.setTradeTime(time.format(System.currentTimeMillis()));
@@ -50,16 +49,16 @@ public class IteratorController {
         two.setBankAccount("other");
         two.setInsertime(dateTime.format(System.currentTimeMillis()));
 
-        OtherDO three=new OtherDO();
+        OtherDO three = new OtherDO();
         three.setId(3);
         three.setName("xiaoming");
         three.setPassword("pwd");
         three.setCreateTime(dateTime.format(System.currentTimeMillis()));
         three.setUpdateTime(dateTime.format(System.currentTimeMillis()));
 
-        map.put("zzx",one);
-        map.put("other",two);
-        map.put("xiaoming",three);
+        map.put("zzx", one);
+        map.put("other", two);
+        map.put("xiaoming", three);
         iteratorService.mapIterator(map);
     }
 }
--- src/main/java/com/zzx/transactions/controller/RefundmentController.java
+++ src/main/java/com/zzx/transactions/controller/RefundmentController.java
@@ -3,11 +3,9 @@ package com.zzx.transactions.controller;
 import com.zzx.transactions.common.CommonResult;
 import com.zzx.transactions.common.ParamterHandler;
 import com.zzx.transactions.common.ProcessHandler;
-import com.zzx.transactions.config.ServiceComponent;
 import com.zzx.transactions.entity.RefundmentDO;
 import com.zzx.transactions.service.RefundmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
