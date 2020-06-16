dd709e7209dfa50e05e01b48736c187ccf97e94f
husky <1101648204@qq.com>
2020-06-16 22:13:41 +0800

    文件解析校验

---
+++ src/main/java/com/zzx/transactions/entity/vo/ArBillAndReceiptVO.java
@@ -0,0 +1,40 @@
+package com.zzx.transactions.entity.vo;
+
+public class ArBillAndReceiptVO {
+    private String billNo;
+    private String billGroupNo;
+    private String receiptNo;
+    private String receiptGroupNo;
+
+    public String getBillNo() {
+        return billNo;
+    }
+
+    public void setBillNo(String billNo) {
+        this.billNo = billNo;
+    }
+
+    public String getBillGroupNo() {
+        return billGroupNo;
+    }
+
+    public void setBillGroupNo(String billGroupNo) {
+        this.billGroupNo = billGroupNo;
+    }
+
+    public String getReceiptNo() {
+        return receiptNo;
+    }
+
+    public void setReceiptNo(String receiptNo) {
+        this.receiptNo = receiptNo;
+    }
+
+    public String getReceiptGroupNo() {
+        return receiptGroupNo;
+    }
+
+    public void setReceiptGroupNo(String receiptGroupNo) {
+        this.receiptGroupNo = receiptGroupNo;
+    }
+}
